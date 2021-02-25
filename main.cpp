e84bc0fcf640409008093acf9ee46f7577a40701
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-25 11:14:10 +0100

    smart pointers with 3 others in all/one

--- main.cpp
+++ main.cpp
@@ -20,8 +20,20 @@
 #include <future>
 #include "numbers.h"
 
+// unikatowe - moze byc tylko 1 na jeden wskaznik..  (znika wskaznik = zostaje zwolniona pamiec)
+// wspoldzielony - usuwany 1 = nic sie nie stanie , usuwany 2 = nic sie nie stanie , usuwany 3 = zostaje zwolnion pamiec jezeli all..
+// slaby - jezeli 2 wspoldzielone zgina to 2 slabe zostana puste (nullptr)..
+
 using namespace std;
 
+sharedfunct(){
+    shared_ptr<int> ptr1 = make_shared<int>(10);
+    shared_ptr<int> ptr2;
+}
+
+
 int main(){
-    
+    sharedfunct();
+
+    return 0;
 }
